61c405fd18934f8dac60513dc9aa2495bdb21649
Wilder Lopes <WSLopes@ecad.org.br>
2018-09-05 18:28:09 -0300

    .

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
 let fields = document.querySelectorAll('#form-user-create [name]');
 let user = {};
-fields.forEach((field, index) => {
-    //console.log(`${index} - ${field.name}`);
-    if (field.name=='gender'){
-        if (field.checked){
-            user[field.name] = field.value;
-        }
-    }else {
-        user[field.name] = field.value;
-    }    
-})
 
-//console.log(user);
+function addLine(dataUser){
+    console.log(dataUser);
+    let tr = document.createElement("tr");
+    tr.innerHTML = `
+        <tr>
+            <td>
+            <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
+            </td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin ? 'Sim' : 'Não'}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>        
+    `;
 
-/*
-document.querySelectorAll('button').forEach((btn)=>{
-    btn.addEventListener('click', (e) => {
-        alert(`clicou no ${btn.className}`);
-    });
-});
-*/
+    document.getElementById('table-users').appendChild(tr);
+}
+
 document.getElementById('form-user-create').addEventListener('submit', (event)=>{
     //para cancelar o submit padrão do formulário
     event.preventDefault();
 
-    console.log(user);
+    fields.forEach((field, index) => {
+        //console.log(`${index} - ${field.name}`);
+        if (field.name=='gender'){
+            if (field.checked){
+                user[field.name] = field.value;
+            }
+        }else {
+            user[field.name] = field.value;
+        }    
+    })    
+
+    //console.log(user);
+    addLine(user);
 });
