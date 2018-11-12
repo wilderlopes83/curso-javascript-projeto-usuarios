7bbf918ed21e9831d0d475adc02ad9812262647a
Wilder Lopes <WSLopes@ecad.org.br>
2018-11-12 18:06:27 -0200

    atualizações das aulas até 12/11/2018

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -54,7 +54,7 @@ class UserController{
             }
     
             if (file){
-            fileReader.readAsDataURL(file);
+                fileReader.readAsDataURL(file);
             }
             else{
                 resolve(`dist/img/boxed-bg.jpg`);
@@ -68,11 +68,21 @@ class UserController{
     getValues(){
 
         let user = {};
+        let isValid = true;
         let fields = this.formEl.elements;
 
         //esses 3 primeiros pontos e colocar o objeto entre [] é uma técnica chamada "spread"
         //isso transforma o objeto em array e os ... significam que não preciso informar os índices... 
         [...fields].forEach((field, index) => {
+
+            //validação se campos obrigatórios preenchidos
+            if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value){
+
+                //inclui a classe has-error no elemento pai do formulário
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+                return false;
+            }
 
             if (field.name=='gender'){
                 if (field.checked){
@@ -84,6 +94,10 @@ class UserController{
                 user[field.name] = field.value;
             }    
         });    
+
+        if (!isValid){
+            return false;
+        }
 
         return new User(
             user.name, 
@@ -108,7 +122,7 @@ class UserController{
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
                 <td>${(dataUser.admin ? 'Sim' : 'Não')}</td>
-                <td>${dataUser.birth}</td>
+                <td>${Utils.dateFormat(dataUser.register)}</td>
                 <td>
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
