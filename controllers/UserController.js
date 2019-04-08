1dba6f64490b2626cf92b1ef1558aea58f8ebb3f
Wilder da Silva Lopes <wilderlopes83@gmail.com>
2019-04-08 18:11:02 -0300

    Exercícios até aula 53

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -216,7 +216,7 @@ class UserController{
                 <td>${Utils.dateFormat(dataUser.register)}</td>
                 <td>
                 <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
                 </td>
         `;
 
@@ -230,6 +230,15 @@ class UserController{
     }    
 
     addEventsTr(tr) {
+
+        tr.querySelector(".btn-delete").addEventListener("click", e => {
+                if (confirm('Tem certeza que deseja excluir?')){
+                    tr.remove();
+                    this.updateCount();
+                }
+            }
+        );
+
         tr.querySelector(".btn-edit").addEventListener("click", e => {
             let json = JSON.parse(tr.dataset.user);
             
